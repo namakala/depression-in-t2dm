b9fa71b52ae3c930eaeedf2c7e2977f037baa53b
lamurian <lamuri.aly8@gmail.com>
2023-09-07 03:04:04 +0700

    Design the analysis pipeline

---
+++ _targets.R
@@ -0,0 +1,32 @@
+# Load packages
+sapply(
+  c("magrittr", "targets", "tarchetypes"),
+  library,
+  character.only = TRUE
+)
+
+# List all data files
+raw <- list.files(
+  "data/raw", full.names = TRUE, recursive = TRUE, pattern = "*.csv"
+) %>%
+  set_names(gsub(x = ., ".*/|.csv|\\W", ""))
+
+# Source all functions
+sapply(
+  list.files("src/R", recursive = TRUE, pattern = "*.R", full.names = TRUE),
+  source
+)
+
+# Set group of variables to iterate the meta analysis
+itergroup <- c(
+  "year", "country", "criteria", "instrument"
+)
+
+# Initiate analysis pipeline
+list(
+  tar_target(file_extract, raw["extracted"], format = "file"),
+  tar_target(tbl, readData(file_extract)),
+  tar_target(pooled_author, iterate(tbl, "author", sm = "PRAW")),
+  tar_target(tbl_clean, tbl),
+  tar_target(pooled_es, iterate(tbl_clean, itergroup, sm = "PRAW"))
+)
